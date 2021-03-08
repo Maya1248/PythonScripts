f9c1e9de31d8cfad9ff0e1364b53b87e79772629
Borna <45570588+ParzivalKnight5@users.noreply.github.com>
2021-03-08 20:19:25 +0100

    Update typeracer_cheat.py

--- typeracer_cheat.py
+++ typeracer_cheat.py
@@ -1,14 +1,6 @@
 import pyautogui as gui
 import time
 
-# Rapid speed growth requires a captcha verification, use the cheat to
-# grow gradually every x + <25%
-# Characters per minute (CPM)
-# x = your current cpm, d = delay (in seconds) between characters
-
-# n = (x + <25%) / 60
-# d = 1 / n
-
 print("Open inspect element, check the text, edit and copy paste it.")
 
 def cheat():
