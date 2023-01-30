6dfdc0c761e443791c17721586a07606b24e2da8
Borna <45570588+ParzivalKnight5@users.noreply.github.com>
2023-01-31 00:47:08 +0100

    Add files via upload

--- Encrypt_Image/encrypt_Image.py
+++ Encrypt_Image/encrypt_Image.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from AesEverywhere import aes256
+import os
 
 def getImageData(image):
     im = Image.open(image)
@@ -51,36 +52,94 @@ def conjureImageFromData(image, name):
 
     im.save(name + ".jpg")
 
+def saveToFile(name, pwd):
+    e = encryptImage(name, pwd)
+            
+    with open(name[0:name.find(".")] + ".image", "w") as f:
+        f.write(str(e))
+    
+def getFromFile(name, pwd):
+    with open(name, "r") as f:
+        e = eval(f.read())
+            
+    d = decryptImage(e, pwd)
+    conjureImageFromData(d, name[0:name.find(".")])
+    
+def fetchFiles(folder):
+    os.chdir(folder)
+    images = [im for im in os.listdir()]
+    os.chdir("..")
+    
+    return images
+
+def read():
+    print("1 - Encrypt an image")
+    print("2 - Decrypt an image")
+    print("3 - Encrypt all images in a folder")
+    print("4 - Decrypt all images in a folder")
+    print("5 - Exit")
 
 def main():
     ch = 0
 
     while True:
-        ch = str(input("1 - Encrypt image \n2 - Decrypt image \n3 - Exit \n>"))
+        ch = str(input(">"))
         
         if ch == "1":
             name = str(input("Name (e.g. image.jpg, image2.png): "))
             pwd = str(input("Password (remember this!): "))
-            
-            e = encryptImage(name, pwd)
-            
-            with open(name[0:name.find(".")] + ".image", "w") as f:
-                f.write(str(e))
+
+            saveToFile(name, pwd)
 
             print("[+] " + name + " encrypted\n")
+            
         elif ch == "2":
             name = str(input("Name (e.g. image.image, image2.image): "))
             pwd = str(input("Password: "))
 
-            with open(name, "r") as f:
-                e = eval(f.read())
+            getFromFile(name, pwd)
+
+            print("[+] " + name + " decrypted\n")
             
-            d = decryptImage(e, pwd)
-            conjureImageFromData(d, name[0:name.find(".")])
         elif ch == "3":
+            name = str(input("Name of folder to encrypt: "))
+            pwd = str(input("Password: "))
+
+            all_images = fetchFiles(name)
+
+            os.mkdir("encrypted_files")
+            os.chdir(name)
+
+            for image in all_images:
+                os.chdir("../encrypted_files")
+                saveToFile("../" + name + "/" + image, pwd)
+
+                print("[+] " + image + " encrypted\n")
+            
+
+        elif ch == "4":
+            name = str(input("Name of folder to decrypt: "))
+            pwd = str(input("Passowrd: "))
+
+            all_files = fetchFiles(name)
+
+            os.mkdir("decrypted_files")
+            os.chdir(name)
+
+            for file in all_files:
+                os.chdir("../decrypted_files")
+                getFromFile("../" + name + "/" + file, pwd)
+                
+                print("[+] " + file + " decrypted\n")
+                
+            
+        elif ch == "5":
             break
+        
         else:
             print("[-] Uknown command\n")
 
+
 if __name__ == "__main__":
+    read()
     main()
